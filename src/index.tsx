d1007c75e82e766b68479fa0cc0be75f64bcaa66
Christopher Bagge <chris@bagge.com>
2021-12-03 11:53:48 -0500

    change URI

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import Pages from './pages'
 import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://server-cba-castronauts.herokuapp.com',
   cache: new InMemoryCache()
 });
 
